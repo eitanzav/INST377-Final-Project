28705807ee093e18c938bbfa6cf10b757c8377b0
Laura-villada <123710079+Laura-villada@users.noreply.github.com>
2023-12-18 08:49:34 -0500

    style changed

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-// Function to search for recipes based on user input
+// Function to search for recipes based on user input and dropdown values
 function searchRecipes() {
     // Get the user input from the search bar
     const query = document.getElementById("recipeSearch").value;
 
-    // Get selected cuisine from the dropdown
-    const selectedCuisine = document.getElementById("cuisineDropdown").value;
+    // Get the selected cuisine from the dropdown
+    const cuisineDropdown = document.getElementById("cuisineDropdown");
+    const selectedCuisine = cuisineDropdown.options[cuisineDropdown.selectedIndex].value;
 
-    // Get selected diet from the dropdown
-    const selectedDiet = document.getElementById("dietDropdown").value;
+    // Get the selected diet from the dropdown
+    const dietDropdown = document.getElementById("dietDropdown");
+    const selectedDiet = dietDropdown.options[dietDropdown.selectedIndex].value;
 
-    // Spoonacular API endpoint
+    // Construct the API URL
     const apiEndpoint = "https://api.spoonacular.com/recipes/complexSearch";
-
     const apiKey = "4b9165fcaea647c68ad3106a63e52749";
-
-    // Build the API URL with user input, cuisine, and diet
     const apiUrl = `${apiEndpoint}?query=${query}&cuisine=${selectedCuisine}&diet=${selectedDiet}&apiKey=${apiKey}`;
 
     // Make a GET request to the Spoonacular API
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            // Process the data and display the results (CUSTOMIZE)
+            // Process the data and display the results
             displayResults(data);
         })
         .catch(error => {
@@ -29,13 +28,29 @@ function searchRecipes() {
         });
 }
 
-// Function to display search results NEED TO CUSTOMIZE VERY BASIC
+// Function to display search results
 function displayResults(data) {
-    // Example: Log the recipe titles to the console
-    data.results.forEach(recipe => {
-        console.log(recipe.title);
-    });
+    // Get the container where you want to display the results
+    const resultsContainer = document.getElementById("results-container");
 
+    // Clear previous results
+    resultsContainer.innerHTML = "";
+
+    // Loop through the results and create a card for each recipe
+    data.results.forEach(recipe => {
+        const card = document.createElement("div");
+        card.classList.add("recipe-card");
+
+        const title = document.createElement("h3");
+        title.textContent = recipe.title;
+
+        const image = document.createElement("img");
+        image.src = recipe.image;
+
+        card.appendChild(title);
+        card.appendChild(image);
+        resultsContainer.appendChild(card);
+    });
 }
 
 // Attach the searchRecipes function to the button click event
